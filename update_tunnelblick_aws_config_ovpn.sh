96fdc0b0fa48e59f1857d958e53e5f61e8497d0b
lilongen <lilongen@gmail.com>
2021-05-17 16:21:48 +0800

    onekey automatically finished

--- update_tunnelblick_aws_config_ovpn.sh
+++ update_tunnelblick_aws_config_ovpn.sh
@@ -2,12 +2,14 @@
 # automatically update tunnelblock client config file remote host
 #
 
+
 # 1. get public ip from terrafrom tfstate file when run terrform plan to create a new eip
 # 2. get public ip from aws cli describe-instances output,
 #    cause last "terraform destory" will destroy eip, and automatically renew a eip to ec2 instance
 #    but it will not listed in eip list
 file_tfstate="./terraform/terraform.tfstate"
 public_ip="$(grep -o '"public_ip": .*' $file_tfstate | awk -F'"' '{print $4}')"
+echo "public_ip: $public_ip"
 if [ "$public_ip" = "" ]; then
   echo "public_ip not found in terraform state!"
   echo "get public_ip using aws cli ..."
@@ -22,6 +24,7 @@ tunnelblick_ovpn_location="/Library/Application Support/Tunnelblick/Shared/"
 ovpn_source_location="/Users/lilongen/onedrive/aws/openvpn/client_config/"
 echo "find candidated ovpn config file ..."
 sudo find "$tunnelblick_ovpn_location" "$ovpn_source_location" -name "*.ovpn" | grep aws-ov-bw | tee candidated.config.files
+
 
 timestamp=$(date "+%Y%m%d.%H%M%S")
 echo "'remote host' in these files will be updated to '${public_ip}' ..."
