c12dc3008612f988a63a2a9bd4f722b1a41fa6b3
lilongen <lilongen@gmail.com>
2021-06-01 22:20:18 +0800

    refactory

--- renew_ec2_eip_and_update_vpn_client_config.sh
+++ renew_ec2_eip_and_update_vpn_client_config.sh
@@ -2,22 +2,20 @@
 # automatically renew ec2 eip and update tunnelblock client config file remote host
 #
 
-echo "disconnect Tunnelblick aws connection ..."
-osascript ./disconnect_tunnelblick_aws_connection.applescript
+run() {
+  cmd=$1
+  desc=$2
+  echo "$desc ..."
+  echo "$cmd"
+  eval "$cmd"
+  echo "${desc} ... done"
+}
+
+run "osascript ./disconnect_tunnelblick_aws_connection.applescript" "disconnect Tunnelblick aws connection"
 sleep 8
-echo "disconnect Tunnelblick aws connection ... done"
-
-echo "renew ec2 eip ..."
-python ./pyexpect_renew_ec2_eip.py
-echo "renew ec2 eip ... done!"
-
-echo "udpate tunnelblick aws openvpn client config file ..."
-./update_tunnelblick_aws_config_ovpn.sh
-echo "udpate tunnelblick aws openvpn client config file ... done!"
-
-echo "restart tunnelblick and auto connect ..."
-osascript ./restart_tunnelblick.applescript
-echo "restart tunnelblick and auto connect ... done!"
+run "python ./pyexpect_renew_ec2_eip.py" "renew ec2 eip"
+run "./update_tunnelblick_aws_config_ovpn.sh" "udpate tunnelblick aws openvpn client config file"
+run "osascript ./restart_tunnelblick.applescript" "restart tunnelblick and auto connect"
 
 echo "finished!"
 
